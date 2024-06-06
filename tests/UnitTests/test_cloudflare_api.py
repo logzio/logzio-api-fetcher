adfde20dd27b5c62de6c2f29ecfaf310c09cb286
8naama <naama.bendalak@logz.io>
2024-06-06 13:14:29 +0300

    add unit tests

---
+++ tests/UnitTests/test_cloudflare_api.py
@@ -0,0 +1,47 @@
+import json
+from pydantic import ValidationError
+import responses
+import unittest
+
+from src.apis.cloudflare.Cloudflare import Cloudflare
+
+
+class TestCloudflareApi(unittest.TestCase):
+    """
+    Test cases for Cloudflare API
+    """
+
+    def test_invalid_setup(self):
+        with self.assertRaises(ValidationError):
+            Cloudflare(cloudflare_account_id="abcd-efg",
+                       url="https://api.cloudflare.com/client/v4/accounts/{account_id}/alerting/v3/history")
+            Cloudflare(cloudflare_bearer_token="mYbeReartOKen",
+                       url="https://api.cloudflare.com/client/v4/accounts/{account_id}/alerting/v3/history")
+            Cloudflare(cloudflare_account_id="abcd-efg", cloudflare_bearer_token="mYbeReartOKen")
+
+    @responses.activate
+    def test_valid_setup(self):
+        # Mock response from Cloudflare API
+        with open("./cloudflare_res_example.json", "r") as res_example_file:
+            res = json.loads(res_example_file.read())
+
+        # First Data Request
+        responses.add(responses.GET, "https://api.cloudflare.com/client/v4/accounts/abcd-efg/alerting/v3/history",
+                      json=res,
+                      status=200)
+
+        # Pagination Request
+        responses.add(responses.GET, "https://api.cloudflare.com/client/v4/accounts/abcd-efg/alerting/v3/history?page=2",
+                      json={"result": [], "result_info": {"page": 2, "total_pages": 1}},
+                      status=200)
+
+        a = Cloudflare(cloudflare_account_id="abcd-efg",
+                       cloudflare_bearer_token="mYbeReartOKen",
+                       url="https://api.cloudflare.com/client/v4/accounts/{account_id}/alerting/v3/history",
+                       next_url="https://api.cloudflare.com/client/v4/accounts/{account_id}/alerting/v3/history?since={res.result.[0].sent}")
+
+        # Test sending request
+        results = a.send_request()
+
+        self.assertEqual(a.url, "https://api.cloudflare.com/client/v4/accounts/abcd-efg/alerting/v3/history?since=2024-05-24T03:22:45.410294Z")
+        self.assertEqual(results, res.get("result"))
---
+++ tests/UnitTests/test_oauth_class.py
@@ -0,0 +1,62 @@
+from pydantic import ValidationError
+import responses
+import unittest
+
+from src.apis.general.Api import ApiFetcher, ReqMethod
+from src.apis.oauth.OAuth import OAuthApi
+
+
+class TestOAuthApi(unittest.TestCase):
+    """
+    Test cases for the OAuth API
+    """
+
+    def test_invalid_setup(self):
+        with self.assertRaises(ValidationError):
+            # missing token_request
+            OAuthApi(data_request=ApiFetcher(url="http://my-data-url"))
+
+            # missing data_request
+            OAuthApi(token_request=ApiFetcher(url="http://my-token-url"))
+
+            # scrape_interval too big
+            OAuthApi(token_request=ApiFetcher(url="http://my-token-url"),
+                     data_request=ApiFetcher(url="http://my-data-url"),
+                     scrape_interval=0)
+
+    def test_headers_setup(self):
+        # No initializing to headers
+        a = OAuthApi(token_request=ApiFetcher(url="http://my-token-url"),
+                     data_request=ApiFetcher(url="http://my-data-url"))
+        self.assertEqual(a.data_request.headers.get("Content-Type"), "application/json")
+
+        # Initializing to headers
+        a = OAuthApi(token_request=ApiFetcher(url="http://my-token-url"),
+                     data_request=ApiFetcher(url="http://my-data-url",
+                                             headers={"Content-Type": "application/gzip"}))
+        self.assertEqual(a.data_request.headers.get("Content-Type"), "application/gzip")
+
+    @responses.activate
+    def test_send_request(self):
+        token_res = {"access_token": "epZliHdLuj", "expires_in": 1}
+        data_res = {"data": [{"msg": "hi"}, {"msg": "hello", "field": 567}]}
+
+        # Mock response from some API
+        responses.add(responses.POST, "http://my-token-url",
+                      json=token_res,
+                      status=200)
+        responses.add(responses.GET, "http://my-data-url",
+                      json=data_res,
+                      status=200)
+
+        a = OAuthApi(token_request=ApiFetcher(url="http://my-token-url", method=ReqMethod.POST),
+                     data_request=ApiFetcher(url="http://my-data-url", response_data_path="data"))
+        result = a.send_request()
+
+        # Ensure we got the needed results
+        self.assertEqual(result, [{"msg": "hi"}, {"msg": "hello", "field": 567}])
+
+        # Test needing to update the token (gave super short expiration in 'token_res')
+        with self.assertLogs("src.apis.oauth.OAuth", level='DEBUG') as log:
+            a.send_request()
+        self.assertIn("DEBUG:src.apis.oauth.OAuth:Sending request to update the access token.", log.output)
