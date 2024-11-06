cfde920e937033404cb2b6ae2e2184479b310ce8
Yotam loewenbach <yotam.loewenbach@logz.io>
2024-11-06 16:30:46 +0200

    Get JWT token for each request

--- src/apis/dockerhub/Dockerhub.py
+++ src/apis/dockerhub/Dockerhub.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta, UTC
 import json
 import logging
+
+import requests
 from pydantic import Field
 
 from src.apis.general.Api import ApiFetcher
@@ -9,55 +11,64 @@ from src.apis.general.StopPaginationSettings import StopPaginationSettings
 
 logger = logging.getLogger(__name__)
 
-class DockerHub(ApiFetcher):
 
-    dockerhub_token: str = Field(frozen=True)
-    account: str = Field(frozen=True)
-    pagination_off: bool = Field(default=False)
-    days_back_fetch: int = Field(default=-1, frozen=True)
-    page_size: int = Field(default=25, ge=1, le=100)
+class DockerHub(ApiFetcher):
+    """
+    :param dockerhub_token: The DockerHub personal access token or password
+    :param pagination_off: True if pagination should be off, False otherwise
+    :param days_back_fetch: Amount of days to fetch back in the first request, Optional (adds a filter on 'from')
+    :param page_size: Number of events to return in a single request (for pagination)
+    """
+    dockerhub_user: str = Field(frozen=True)
+    dockerhub_token: str = Field(frozen=False)
+    days_back_fetch: int = Field(default=1, frozen=True)
 
     def __init__(self, **data):
-        page_size = data.pop('page_size', 25)
-
-        res_data_path = "events"
+        # Initialize page size for number of events to return in a single request
+        # Configure the request
+        res_data_path = "logs"
         headers = {
             "Content-Type": "application/json",
-            "Authorization": f"Bearer {data.get('dockerhub_token')}"
         }
-        params = {
-            "page_size": page_size
-        }
-        next_params = {
-            "page_size": page_size,
-            "page": "{res.next_page}"
-        }
-        pagination = None
-        if not data.get("pagination_off"):
-            pagination = PaginationSettings(type="url",
-                                            url_format="&page={res.next_page}",
-                                            stop_indication=StopPaginationSettings(field="next_page",
-                                                                                   condition="equals",
-                                                                                   value=None))
-        super().__init__(headers=headers, params=params, next_params=next_params, pagination=pagination, response_data_path=res_data_path, **data)
+        super().__init__(headers=headers, response_data_path=res_data_path, **data)
 
-        if self.days_back_fetch > 0:
-            self._initialize_params_date()
+        self._initialize_params()
 
-    def _initialize_params_date(self):
-
+    def _initialize_params(self):
+        """
+        Initialize the first request's 'from' parameter.
+        """
         try:
             from_date = (datetime.now(UTC) - timedelta(days=self.days_back_fetch)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-            self.params["from"] = from_date
+            if "?" in self.url:
+                self.url += f"&from={from_date}&page_size=100"
+            else:
+                self.url += f"?from={from_date}&page_size=100"
         except Exception as e:
-            logger.error(f"Failed to update 'from' filter in the request params: {self.params}. Sending {self.name} "
+            logger.error(f"Failed to update 'from' filter in the request params:. Sending {self.name} "
                          f"request with no date filter. Error: {e}")
 
+    def _get_jwt_token(self):
+        url = "https://hub.docker.com/v2/users/login"
+        payload = {
+            "username": self.dockerhub_user,
+            "password": self.dockerhub_token
+        }
+        headers = {"Content-Type": "application/json"}
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            response.raise_for_status()
+            token_response = response.json()
+            return token_response.get("token")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Failed to get JWT token: {e}")
+
     def send_request(self):
-
+        """
+        Sends request using the super class and handles pagination if needed.
+        :return: all the responses that were received
+        """
+        jwt_token = self._get_jwt_token()
+        self.headers["Authorization"] = f"Bearer {jwt_token}"
         data = super().send_request()
-
-        if data:
-            latest_timestamp = data[-1].get("timestamp")
-            self.params["from"] = latest_timestamp
         return data
