d066c1e8abdf7cfa4f5dd67e2eba316b255380a0
Yotam loewenbach <yotam.loewenbach@logz.io>
2024-11-07 14:35:36 +0200

    Cache jwt token + improve `_initialize_params`

--- src/apis/dockerhub/Dockerhub.py
+++ src/apis/dockerhub/Dockerhub.py
@@ -1,54 +1,52 @@
 from datetime import datetime, timedelta, UTC
 import json
 import logging
-
 import requests
 from pydantic import Field
-
 from src.apis.general.Api import ApiFetcher
-from src.apis.general.PaginationSettings import PaginationSettings
-from src.apis.general.StopPaginationSettings import StopPaginationSettings
 
 logger = logging.getLogger(__name__)
 
 
 class DockerHub(ApiFetcher):
     """
+    :param dockerhub_user: The DockerHub username
     :param dockerhub_token: The DockerHub personal access token or password
-    :param pagination_off: True if pagination should be off, False otherwise
-    :param days_back_fetch: Amount of days to fetch back in the first request, Optional (adds a filter on 'from')
+    :param days_back_fetch: Number of days to fetch back in the first request, Optional (adds a filter on 'from')
     :param page_size: Number of events to return in a single request (for pagination)
     """
     dockerhub_user: str = Field(frozen=True)
     dockerhub_token: str = Field(frozen=False)
-    days_back_fetch: int = Field(default=1, frozen=True)
+    days_back_fetch: int = Field(default=-1, frozen=True)
+    _jwt_token: str = None
 
     def __init__(self, **data):
-        # Initialize page size for number of events to return in a single request
-        # Configure the request
         res_data_path = "logs"
         headers = {
             "Content-Type": "application/json",
         }
         super().__init__(headers=headers, response_data_path=res_data_path, **data)
-
         self._initialize_params()
 
     def _initialize_params(self):
-        """
-        Initialize the first request's 'from' parameter.
-        """
         try:
-            from_date = (datetime.now(UTC) - timedelta(days=self.days_back_fetch)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            params = {"page_size": 100}
+            if self.days_back_fetch > 0:
+                from_date = (datetime.now(UTC) - timedelta(days=self.days_back_fetch)).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+                params["from"] = from_date
+            query_string = "&".join([f"{key}={value}" for key, value in params.items()])
             if "?" in self.url:
-                self.url += f"&from={from_date}&page_size=100"
+                self.url += f"&{query_string}"
             else:
-                self.url += f"?from={from_date}&page_size=100"
+                self.url += f"?{query_string}"
         except Exception as e:
-            logger.error(f"Failed to update 'from' filter in the request params:. Sending {self.name} "
-                         f"request with no date filter. Error: {e}")
+            logger.error(
+                f"Failed to update request params. Sending {self.name} request with default params. Error: {e}")
 
     def _get_jwt_token(self):
+        if self._jwt_token:
+            return self._jwt_token
+
         url = "https://hub.docker.com/v2/users/login"
         payload = {
             "username": self.dockerhub_user,
@@ -59,15 +57,12 @@ class DockerHub(ApiFetcher):
             response = requests.post(url, json=payload, headers=headers)
             response.raise_for_status()
             token_response = response.json()
-            return token_response.get("token")
+            self._jwt_token = token_response.get("token")
+            return self._jwt_token
         except requests.exceptions.RequestException as e:
             logger.error(f"Failed to get JWT token: {e}")
 
     def send_request(self):
-        """
-        Sends request using the super class and handles pagination if needed.
-        :return: all the responses that were received
-        """
         jwt_token = self._get_jwt_token()
         self.headers["Authorization"] = f"Bearer {jwt_token}"
         data = super().send_request()
