30a8cbd14468a9a6b2bd945e848541774dab114b
Yotam loewenbach <yotam.loewenbach@logz.io>
2025-01-08 17:32:16 +0700

    Add `api_e2e_test.py`

---
+++ e2e_tests/api_e2e_test.py
@@ -0,0 +1,55 @@
+import os
+import threading
+import random
+import string
+import unittest
+from os.path import join, abspath, dirname
+
+from dotenv import load_dotenv
+
+from src.main import main
+from e2e_tests.utils.config_utils import update_config_tokens, delete_temp_files, validate_config_tokens
+from e2e_tests.utils.log_utils import search_data
+
+
+class ApiE2ETest(unittest.TestCase):
+
+    def setUp(self):
+        self.test_type = f"api-fetcher-e2e-test-{self.generate_random_string()}"
+        os.environ["TEST_TYPE"] = self.test_type
+        dotenv_path = join(abspath(dirname(__file__)), '.env')
+        load_dotenv(dotenv_path=dotenv_path)
+        self.token_map = self.get_token_map()
+        self.config_path = self.get_config_path()
+        self.temp_config_path = update_config_tokens(self.config_path, self.token_map)
+        validate_config_tokens(self.token_map)
+        self.module_specific_setup()
+
+    def tearDown(self):
+        self.module_specific_teardown()
+        delete_temp_files()
+
+    @staticmethod
+    def generate_random_string(length=6):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+    def run_main_program(self, test=False):
+        thread = threading.Thread(target=main, kwargs={"conf_path": self.temp_config_path, "test": test})
+        thread.daemon = True
+        thread.start()
+        thread.join(timeout=60)
+
+    def search_logs(self, query):
+        return search_data(query)
+
+    def get_token_map(self):
+        raise NotImplementedError("Subclasses should implement this method")
+
+    def get_config_path(self):
+        raise NotImplementedError("Subclasses should implement this method")
+
+    def module_specific_setup(self):
+        pass
+
+    def module_specific_teardown(self):
+        pass
--- e2e_tests/apis/test_e2e_dockerhub.py
+++ e2e_tests/apis/test_e2e_dockerhub.py
@@ -1,75 +1,51 @@
-import os
-import threading
-import random
-import string
-from os.path import abspath, dirname
 import unittest
-import docker
-import requests
-from datetime import datetime, timedelta
+from os.path import abspath, dirname
 
 from dotenv import load_dotenv
 
-from src.main import main
-from e2e_tests.utils.config_utils import update_config_tokens, delete_temp_files, validate_config_tokens
-from e2e_tests.utils.log_utils import search_data
+from e2e_tests.api_e2e_test import ApiE2ETest
+import docker
+import requests
+from datetime import datetime, timedelta
 
 DOCKER_IMAGE = "logzio/dummy-docker-image"
 DOCKER_TAG = "latest"
 
 
-def generate_random_string(length=6):
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+class TestDockerhubE2E(ApiE2ETest):
 
-
-def was_tag_pushed_recently(image, tag, days=14):
-    url = f"https://hub.docker.com/v2/repositories/{image}/tags/{tag}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        tag_info = response.json()
-        last_updated = datetime.strptime(tag_info['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        return datetime.utcnow() - last_updated < timedelta(days=days)
-    return False
-
-
-class TestDockerhubE2E(unittest.TestCase):
-    """
-    Test data arrived to logzio
-    """
-
-    def setUp(self):
+    def module_specific_setup(self):
         self.client = docker.from_env()
-        self.curr_path = abspath(dirname(__file__))
-        self.config_path = f"{self.curr_path}/testdata/valid_dockerhub_config.yaml"
         self.dockerfile_path = f"{self.curr_path}/testdata/test.dockerfile"
-        self.test_type = f"dockerhub-audit-test-{generate_random_string()}"
-        os.environ["TEST_TYPE"] = self.test_type
-        self.token_map = {
+        if not self.was_tag_pushed_recently(DOCKER_IMAGE, DOCKER_TAG):
+            self.client.images.build(path=self.curr_path, dockerfile=self.dockerfile_path,
+                                     tag=f"{DOCKER_IMAGE}:{DOCKER_TAG}")
+            self.client.images.push(DOCKER_IMAGE, tag=DOCKER_TAG)
+
+    def get_token_map(self):
+        return {
             "apis.0.dockerhub_token": "DOCKERHUB_TOKEN",
             "apis.0.dockerhub_user": "DOCKERHUB_USER",
             "logzio.token": "LOGZIO_SHIPPING_TOKEN",
             "apis.0.additional_fields.type": "TEST_TYPE"
         }
-        load_dotenv(dotenv_path='testdata/.env')
-        validate_config_tokens(self.token_map)
-        self.temp_config_path = update_config_tokens(self.config_path, self.token_map)
 
-        if not was_tag_pushed_recently(DOCKER_IMAGE, DOCKER_TAG):
-            self.client.images.build(path=self.curr_path, dockerfile=self.dockerfile_path,
-                                     tag=f"{DOCKER_IMAGE}:{DOCKER_TAG}")
-            self.client.images.push(DOCKER_IMAGE, tag=DOCKER_TAG)
+    def get_config_path(self):
+        self.curr_path = abspath(dirname(__file__))
+        return f"{self.curr_path}/testdata/valid_dockerhub_config.yaml"
 
-    def tearDown(self):
-        delete_temp_files()
+    def was_tag_pushed_recently(self, image, tag, days=14):
+        url = f"https://hub.docker.com/v2/repositories/{image}/tags/{tag}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            tag_info = response.json()
+            last_updated = datetime.strptime(tag_info['last_updated'], "%Y-%m-%dT%H:%M:%S.%fZ")
+            return datetime.utcnow() - last_updated < timedelta(days=days)
+        return False
 
     def test_dockerhub_audit_logs_e2e(self):
-        thread = threading.Thread(target=main,
-                                  kwargs={"conf_path": self.temp_config_path, "test": False})
-        thread.daemon = True
-        thread.start()
-        thread.join(timeout=60)
-
-        logs = search_data(f"type:{self.test_type}")
+        self.run_main_program()
+        logs = self.search_logs(f"type:{self.test_type}")
         self.assertTrue(logs)
         self.assertTrue(all([log.get("_source").get("eventType") == "auditevents" for log in logs]))
 
